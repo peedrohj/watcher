ae4fd8d97476b3d42183acdd3ee3933ab4d310f7
Pedro Farias <phjfg@cesar.org.br>
2024-09-15 20:34:50 -0300

    feat (grafana): updates grafana command to use a variable

--- code/services/grafana/run.sh
+++ code/services/grafana/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 enable_logs="${ENABLE_LOGS_GRAFANA:=false}"
+command="./grafana server"
+
 cd /app/grafana/bin
 
 if [ "$enable_logs" = "true" ]; then
     echo "Starting Grafana with logs enabled..."
-    ./grafana server
+    $command
 else
-    ./grafana server > /dev/null 2>&1 &
+    $command > /dev/null 2>&1 &
 fi
 
 echo "Grafana is up on port: 3000"
