594ea3b91cb5de2917d1e1ee08c875f2564f2a10
Pedro Farias <phjfg@cesar.org.br>
2024-09-15 23:08:04 -0300

    feat (prometheus): create startup script

---
+++ code/services/prometheus/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. /app/services/logger.sh
+
+enable_logs="${ENABLE_LOGS_PROMETHEUS:=false}"
+config_file="/app/services/prometheus/config.yaml"
+command="./prometheus \
+      --web.enable-remote-write-receiver \
+      --enable-feature=otlp-write-receiver \
+      --enable-feature=exemplar-storage \
+      --enable-feature=native-histograms \
+      --storage.tsdb.path=/data/prometheus \
+      --config.file=${config_file}
+"
+
+cd /app/prometheus
+logger "$enable_logs" "$command" &
+
--- code/services/run.sh
+++ code/services/run.sh
@@ -2,5 +2,6 @@
 
 sh /app/services/grafana/run.sh &
 sh /app/services/otel/run.sh &
+sh /app/services/prometheus/run.sh &
 
 sleep infinity
